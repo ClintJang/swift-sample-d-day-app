298a0d0ff5e056ecc129f9f139d4e50a9c3d7f47
Clint Jang <jang.wangsu@gmail.com>
2019-05-20 21:01:34 +0900

    Feature : Add a project - I created a project with the name JWSDDay.

---
+++ JWSDDay/JWSDDay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JWSDDay
+//
+//  Created by ClintJang on 20/05/2019.
+//  Copyright © 2019 ClintJang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
